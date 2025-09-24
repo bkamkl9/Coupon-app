90f11864145d6f912ed143ee4bfff35bcbb4c61c
Kamil <kamil.bartczak@infermedica.com>
2025-09-24 11:33:36 +0000

    add deployment for mobile app

--- frontend/apps/admin/vite.config.ts
+++ frontend/apps/admin/vite.config.ts
@@ -1,9 +1,12 @@
 import { fileURLToPath, URL } from 'node:url'
-
+import { existsSync, readFileSync, unlinkSync, writeFileSync } from "node:fs";
+import path from "node:path";
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
+
+const root = path.resolve();
 
 // https://vite.dev/config/
 export default defineConfig({
