69bca395fa06ce3577d1604035c649b9c4cd7e22
Kamil <kamil.bartczak@infermedica.com>
2025-09-26 19:49:45 +0000

    prepare app file

---
+++ frontend/apps/mobile/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.apiokazje.app',
+  appName: '@workspace/mobile',
+  webDir: 'dist'
+};
+
+export default config;
--- frontend/apps/mobile/vite.config.ts
+++ frontend/apps/mobile/vite.config.ts
@@ -20,5 +20,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-  base: '/Coupon-app/mobile/',
+  base: process.env.LOCAL ? '/' : '/Coupon-app/mobile/',
 })
